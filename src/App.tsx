069f482690b636ab6e6e1010786a6e2d631fafd3
Liedel <liedel75@gmail.com>
2022-10-06 17:41:03 +0200

    Props,Hooks,Routes & form

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, {FunctionComponent} from 'react';
+import PokemonList from "./pages/pokemon-list";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import PokemonDetail from "./pages/pokemon-detail";
+import pageNotFound from "./pages/page-not-found";
+import PokemonEdit from "./pages/pokemon-edit";
+
+const App: FunctionComponent = () => {
+
+    return (
+        <Router>
+            <div>
+                {/*Barre de navigation*/}
+                <nav>
+                    <div className="nav-wrapper teal">
+                        <Link to="/" className="brand-logo center">Pokédex</Link>
+                    </div>
+                </nav>
+                {/*Les routes*/}
+                <Switch>
+                    <Route exact path="/" component={PokemonList}/>
+                    <Route exact path="/pokemons" component={PokemonList}/>
+                    <Route exact path="/pokemons/edit/:id" component={PokemonEdit}/>
+                    <Route path="/pokemons/:id" component={PokemonDetail}/>
+                    <Route component={pageNotFound}/>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
